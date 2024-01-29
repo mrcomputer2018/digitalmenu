c3d43fb819068eca92131570ce6cdfa363c654ed
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-01-29 10:02:10 -0300

    criando estrutura base

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import './App.css';
+import Home from './pages/home/home';
+
+import './style/global.css';
 
 function App() {
     return (
         <div className="App">
-        
+           <Home />
         </div>
     );
 }
