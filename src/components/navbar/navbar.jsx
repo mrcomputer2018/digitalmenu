44ec18ca86c8caa214bd770684f0a12ed1c21114
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-01-29 10:56:00 -0300

    Refactor navbar component to include logo and bag icon

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,11 +1,24 @@
+import Button from "../button/button";
+
+import "./navbar.css";
+import logo from "../../assets/logo.png"
+import bag from "../../assets/bag.png"
+
 function Navbar() {
     return (
-        <>
-            <div>
-                menu navbar...
-                <img src="" alt="" />
+        <div className="navbar-container">
+            <img className="logo" src={ logo } alt="imagem do logotipo" />
+
+            <div className="navabr-menu">
+                <a href="#">Historico</a>
+
+                <Button>
+                    <img className="icon" src={ bag } alt="imagem de uma sacola" />
+                    Sacola
+                </Button>
             </div>
-        </>
+        </div>
+
     )
 }
 
