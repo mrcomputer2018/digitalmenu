707c7a32b7a6403922d67b6ab5b20940d49f11b8
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-01-29 15:50:05 -0300

    Add navbar and title components to checkout page

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -1,8 +1,20 @@
+import Navbar from "../../components/navbar/navbar";
+import Title from "../../components/title/title";
+
 function Checkout() {
+    const titulo = "Finalizar Pedido";
+    const subtitulo = "Revise os itens do seu pedido e clique em finalizar";
+
     return (
-        <div>
-            <h1>Checkout</h1>
-        </div>
+        <d>
+            <Navbar />
+
+            <div className="container">
+                <div>
+                     <Title titulo={ titulo } subtitulo={ subtitulo } />
+                </div>
+            </div>
+        </d>
     );
 }
 
